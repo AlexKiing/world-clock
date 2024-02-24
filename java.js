2663e454990b4bb788d79f9bf31fef24a44e7979
AlexKiing <120453772+AlexKiing@users.noreply.github.com>
2024-02-24 14:06:54 +0200

    Update java.js

--- java.js
+++ java.js
@@ -43,7 +43,7 @@ function updateCity(event){
     </div>
     <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}<small>
     </div>
-    </div>`;
+    </div> <a href="/">All cities</a>`;
     
     }
 (updateTime); setInterval(updateTime, 1000);
